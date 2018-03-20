b3f092fd12b8ddc3a54deddecf8d800e42bc6a67
Michael Pobega <pobega@gmail.com>
2018-03-20 11:19:04 -0400

    Trying to pass args to my caller
    Currently unable to pass args to my caller.
    
    See under the TODO. I keep getting the default nav page.

--- t1_scrape.js
+++ t1_scrape.js
@@ -1,17 +1,36 @@
 function(c, a) { // t:#s.username.target
+  function cr(z) { // simple function to check for corruption in a string
+    let r = /^[A-Za-z0-9_|:\" ]+$/g    // This regexp checks for corruption.
+    return r.test(z)
+  }
+  // Get this corp's navigation
   do {
-    var fp = a.t.call().split("\n"),   // Call front page and split by newline,
-        n = fp[fp.length-1],           // just so we can get the last line.
+    var f = a.t.call().split("\n"),  // Call front page and split by newline,
+      n = f[f.length-1]             // just so we can get the last line.
 
-        [d,e] = n.split(" | "),        // This splits our navigation pages into
-                                       // the two separate page names we need.
+  } while (!(cr(n)))
 
-        re = /^[A-Za-z0-9_|\s]+$/g,    // This regexp checks for corruption.
+  var [k,l] = n.split(" | ")          // This splits our navigation pages into
+                                      // the two separate page names we need.
 
-        y = re.test(n)                 // Run it against the nav page to avoid
-                                       // having corrupt navigation.
+  do {
+    var f = a.t.call({}).split(" "),  // Call front page and split by newline,
+      n = f[f.length-1]             // just so we can get the last line.
 
-  } while (!y)
-  return {ok:true, msg:n}  // This should only be hit if its good :)
-                           // Now we have our navigation.
+  } while (!(cr(n)))
+
+  var [j,m] = n.split(":")
+
+  // TODO: pass args
+  do {
+    var args = [{j:m}]
+      return a.t.call(args)
+      f = a.t.call(args).split("\n"),  // Call front page and split by newline,
+      n = f[f.length-1]             // just so we can get the last line.
+
+    return  n
+  } while (!(cr(n)))
+
+  return {ok:true, msg:j}         // This should only be hit if its good :)
+                                      // Now we have our navigation.
 }
