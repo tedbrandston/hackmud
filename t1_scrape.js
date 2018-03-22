3cdfe89a1d1555cc3a64276b5ca67188ae115602
Michael Pobega <pobega@gmail.com>
2018-03-22 00:20:53 -0400

    Functionalized everything
    Currently trying to functionalize all the logic to save some space.
    
    Still doesn't work.

--- t1_scrape.js
+++ t1_scrape.js
@@ -1,36 +1,34 @@
 function(c, a) { // t:#s.username.target
-  function cr(z) { // simple function to check for corruption in a string
-    let r = /^[A-Za-z0-9_|:\" ]+$/g    // This regexp checks for corruption.
-    return r.test(z)
+
+  function _call(args) { return a.t.call(args) }
+
+  function cr(z) {
+  // simple function to check for corruption in a string or array
+    let r = /^[A-Za-z0-9_|:\" ]+$/g
+    switch (z.constructor) {
+      case String:
+        return r.test(z)
+      case Array:
+        for (var i in z) {
+          if (!r.test(i))
+            return false
+        }
+        return true
+    }
+    return {ok:false, msg:"not a string or array"}
   }
-  // Get this corp's navigation
-  do {
-    var f = a.t.call().split("\n"),  // Call front page and split by newline,
-      n = f[f.length-1]             // just so we can get the last line.
 
-  } while (!(cr(n)))
+  function call(rgx, args) {
+  // execute a regex on a target command output
+    let tmp,pmt
+    do {
+      tmp = _call(args)
+      pmt = rgx.exec(tmp)
+    } while (!cr("test code"))
+    return pmt
+  }
 
-  var [k,l] = n.split(" | ")          // This splits our navigation pages into
-                                      // the two separate page names we need.
+  return call(/([\w])/g)
 
-  do {
-    var f = a.t.call({}).split(" "),  // Call front page and split by newline,
-      n = f[f.length-1]             // just so we can get the last line.
-
-  } while (!(cr(n)))
-
-  var [j,m] = n.split(":")
-
-  // TODO: pass args
-  do {
-    var args = [{j:m}]
-      return a.t.call(args)
-      f = a.t.call(args).split("\n"),  // Call front page and split by newline,
-      n = f[f.length-1]             // just so we can get the last line.
-
-    return  n
-  } while (!(cr(n)))
-
-  return {ok:true, msg:j}         // This should only be hit if its good :)
-                                      // Now we have our navigation.
+  return {ok:true, msg:k}         // Exit goodly
 }
